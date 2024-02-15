569f21f58208de00bdb744a9e11ba250a177710a
Tarik <tarik.badawy@studium.uni-hamburg.de>
2024-02-15 14:40:18 +0100

    feat: list pdfs in directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+use std::ffi::OsStr;
+use std::path;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+#[derive(PartialEq)]
+enum Commands {
+    /// does testing things
+    List,
+    Get,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    if cli.command == Commands::List {
+        if let Some(files) = get_pdfs(path::Path::new("sheet-notes")) {
+            files
+                .iter()
+                .filter_map(|x| x.file_name())
+                .filter_map(|x| x.to_str())
+                .for_each(|x| println!("{x}"));
+        } else {
+            eprint!("Could not read files in dir");
+        }
+    }
+}
+
+fn get_pdfs(dir: &path::Path) -> Option<Vec<path::PathBuf>> {
+    if !dir.is_dir() {
+        return None;
+    }
+
+    let result: Vec<_> = dir
+        .read_dir()
+        .unwrap()
+        .filter_map(|entry| match entry {
+            Ok(entry) => Some(entry.path()),
+            Err(_) => None,
+        })
+        .filter(|entry| entry
+                .extension()
+                .and_then(OsStr::to_str)
+                .eq(&Some("pdf")))
+        .collect();
+    return Some(result)
 }
